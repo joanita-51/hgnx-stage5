ffde76ed19c3846eb76a44ff668100e0762f1ad1
joanita-51 <nakityojoanita51@gmail.com>
2023-09-29 23:48:25 +0300

    sending the video url

--- express.js
+++ express.js
@@ -4,6 +4,7 @@ const path = require('path')
 const multer = require('multer');
 const port = 3000;
 
+
 const videoStorage = multer.diskStorage({
     //destination to store the video
     destination: 'videos',
@@ -24,10 +25,20 @@ const videoUpload = multer({
         cb(undefined,true)
     }
 })
-app.post('/upload', videoUpload.single('video'), (req,res)=>{
-    res.send(req.file)
-},(error,req,res,next)=>{
-    res.status(400).send({error: error.message})
+app.use('/videos', express.static('videos'))
+app.post('/upload', videoUpload.single('video'), async(req,res)=>{
+    try {
+        if(!req.file){
+            return res.status(400).send({error: 'No video file uploaded'})
+        }
+        const videoFileName = req.file.filename
+        const videoUrl = `/videos/${videoFileName}`
+        res.status(200).send({videoUrl})
+
+    } catch (error) {
+        res.status(400).send({error: error.message})
+    }
+
 })
 app.get('/',(req,res)=>{
     res.send("hello there")
